efc7558980d08fd69029d00e8d26a76309c290d0
xinmu <2953282238@qq.com>
2023-04-12 23:27:57 +0800

    css开发环境搭建

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,18 +1,19 @@
 const path = require("path");
 
 // 公共配置
-const postConfig = require("./config/webpackCommon");
+const postConfig = require("./webpackLoader.js");
 
 // 第三方库
+const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-module.exports = {
-  mode: "development",
+
+const commonConfig = {
+  mode:"development",
   entry: "./src/js/index.js",
   output: {
-    publicPath: "./",
     filename: "bundle[contenthash].js",
-    path: path.resolve(__dirname, "./build"),
+    path: path.resolve(__dirname, "../build"),
     clean: true,
   },
   module: {
@@ -48,12 +49,22 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      title: "cssMaster",
       template: "./public/index.html",
     }),
+    new CleanWebpackPlugin(),
   ],
-  devServer: {
-    hot: true,
-  },
+};
+
+const prodConfig = require("./webpack.prod");
+const devConfig = require("./webpack.dev");
+
+//这里导出一个对象 在运行webpack的时候 它会调用 并且把 终端的指令转递过来
+module.exports = function (env) {
+  const isProduction = env.production;
+  console.log(env);
+  process.env.production = isProduction;
+  const config = isProduction ? prodConfig : devConfig;
+  return merge(commonConfig, config);
 };
---
+++ config/webpack.dev.js
@@ -0,0 +1,9 @@
+const isProduction = false;
+
+module.exports = {
+    mode:'development',
+    devtool: 'inline-source-map',
+    devServer:{
+        hot:true
+    },
+}
